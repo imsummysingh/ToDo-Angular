3179c0122d66a58db5c1e09f4ecc9c336cfb0fc9
Summy Singh <summysingh86@gmail.com>
2020-07-26 01:58:20 +0530

    code added

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  public counter=0;
+  public list=[];
+
+  public newTask;
+
+  public addTask(){
+    this.list.push(this.newTask);
+    this.newTask='';
+  }
+
+  public deleteTask(index){
+    this.list.splice(index,1);
+  }
+
+  public increment(){
+    this.counter+=1;
+  }
+
+  public addToList(){
+    this.list.push(1);
+  }
+
+}
